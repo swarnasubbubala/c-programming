288f01f7060424134ae6579cb60df6b2c296e8f6
swarnasubbubala <swarnabalamani1971@gmail.com>
2025-08-29 12:03:28 +0530

    Add files via upload

---
+++ Q2.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	printf("hello guys how are you");
+	return 0;
+	
+}
---
+++ swarnaQ1.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	char a[20];
+	scanf("%s",a);//swarna
+	printf("%s",a);//swarna
+	
+}
